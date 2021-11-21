d83866489c01fbb11e22b6558b77b4c4105e6a17
Yogiraj Pujari <mags749@yahoo.com>
2021-11-21 15:32:06 -0500

    code(structure): Initial Draft

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+</script>
+
+<main>
+  <nav class="bg-white shadow-lg text-gray-100">
+    <div class="container mx-auto">
+      <div class="sm:flex">
+        <a href="#" class="text-gray-100 text-3xl font-bold p-3">APP LOGO</a>
+
+        <!-- Menus -->
+        <div class="ml-55 mt-4">
+          <ul class="text-gray-100 sm:self-center text-xl">
+            <li class="sm:inline-block">
+              <a href="#" class="p-3 hover:text-red-900">About</a>
+            </li>
+            <li class="sm:inline-block">
+              <a href="#" class="p-3 hover:text-red-900">Services</a>
+            </li>
+            <li class="sm:inline-block">
+              <a href="#" class="p-3 hover:text-red-900">Blog</a>
+            </li>
+            <li class="sm:inline-block">
+              <a href="#" class="p-3 hover:text-red-900">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
