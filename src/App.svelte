25b63adc6b567aba380f1566a9fdf924c2462277
Yogiraj Pujari <mags749@yahoo.com>
2021-11-22 16:31:53 -0500

    code(slider): Adding images and slider

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,12 @@
 <script>
+  import Header from "./components/Header/Header.svelte";
+  import Main from "./components/Main/Main.svelte";
+  import Footer from "./components/Footer/Footer.svelte";
 </script>
 
-<main>
-  <nav class="bg-white shadow-lg text-gray-100">
-    <div class="container mx-auto">
-      <div class="sm:flex">
-        <a href="#" class="text-gray-100 text-3xl font-bold p-3">APP LOGO</a>
-
-        <!-- Menus -->
-        <div class="ml-55 mt-4">
-          <ul class="text-gray-100 sm:self-center text-xl">
-            <li class="sm:inline-block">
-              <a href="#" class="p-3 hover:text-red-900">About</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href="#" class="p-3 hover:text-red-900">Services</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href="#" class="p-3 hover:text-red-900">Blog</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href="#" class="p-3 hover:text-red-900">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>
-</main>
+<Header />
+<Main />
+<Footer />
 
 <style global lang="postcss">
   @tailwind base;
---
+++ src/components/Main/Main.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Carousel from "../common/Carousel.svelte";
+  const images = [
+    { path: "images/slide1.jpg", id: "slide1" },
+    { path: "images/slide2.jpg", id: "slide2" },
+    { path: "images/slide3.jpg", id: "slide3" },
+    { path: "images/slide4.jpg", id: "slide4" },
+  ];
+</script>
+
+<main>
+  <Carousel {images} />
+</main>
